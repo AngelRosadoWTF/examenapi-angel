7bf815c4f5f13aebb0a8cbc43f8f563890243ad5
AngelRosadoWTF <angsoft05@gmail.com>
2025-11-14 20:59:07 -0600

    1
    1

--- seeds/seed_users_products.sql
+++ seeds/seed_users_products.sql
@@ -1,6 +1,6 @@
 USE purchases_db;
 
-INSERT INTO users (name) VALUES ('Carlos'), ('Manuel'), ('Ana');
+INSERT INTO users (name) VALUES ('Carlos'), ('Manuel'), ('Ana'), ('Roberto'), ('Diego');
 
 INSERT INTO products (name, price, stock) VALUES
 ('Cloro', 50.25, 20),
@@ -8,4 +8,7 @@ INSERT INTO products (name, price, stock) VALUES
 ('Producto 1', 30.00, 5),
 ('Producto 2', 45.00, 2),
 ('Producto 3', 100.00, 1),
-('Producto 4', 10.00, 100);
+('Producto 4', 10.00, 100),
+('Cereal', 30.00, 10),
+('Leche', 20.00, 4),
+('Pan', 15.00, 5);
